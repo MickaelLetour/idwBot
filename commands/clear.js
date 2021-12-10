eb5032771024ebb92f916687f5147460956440bd
MickaelLetour <mickael.letour@gmail.com>
2021-12-10 21:16:21 +0100

    Refactoring complet, ajout commentaire, ajout fichier README, ajout link du bot

--- commands/clear.js
+++ commands/clear.js
@@ -1,8 +1,9 @@
-const { DiscordAPIError } = require("discord.js")
-
 module.exports = {
+    //information of module
     name : 'clear',
     description : 'Clear messages!',
+
+    //function excute when command matches
     async execute(message, args){
         if(!args[0]){
             return message.reply("Rentrez le nombre de messages a supprimer")
@@ -13,6 +14,7 @@ module.exports = {
         } else if(args[0] < 1){
             return message.reply("Vous devez supprimer au moins 1 message")
         } else {
+            //got the number of user message and delete the messages if the messages dates less than 14 days
             await message.channel.messages.fetch({limit: args[0]})
                 .then(messages => { 
                     message.channel.bulkDelete(messages, true);
--- commands/leave.js
+++ commands/leave.js
@@ -1,12 +1,20 @@
+const play = require ('./play');
+
 module.exports = {
+     //information of module
     name : 'leave',
     description : 'Stop the bot and leave the channel',
+
+    //function excute when command matches
     async execute(message, args){
-        const voiceChannel = message.member.voice.channel;
+        const voiceChannel = message.member.voice.channel;// the vocalChannel of user
         
+        //Bot leave the channel
         if (!voiceChannel) {
             return message.channel.send('Vous devex être dans un salon vocal pour exécuter cette commande')
         } else {
+            play.queue =[];
+            console.log(play.queue);
             await voiceChannel.leave();
             await message.channel.send('Leaving channel :smiling_face_with_tear:');
         }
